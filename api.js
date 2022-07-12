b24da4b0beb9a261b35dde2053660e72abbdd6f2
Phung Pha <phungmac@Phungs-MacBook-Pro.local>
2022-07-12 16:23:36 -0500

    struggling with view

--- api.js
+++ api.js
@@ -1,25 +1,31 @@
 export const Api = (() => {
-  const baseUrl = "http://localhost:4232";
-  const contactPath = "contacts";
+  // const baseUrl = "http://localhost:4232";
+  // const contactPath = "contacts";
 
-  const getContacts = () =>
-  fetch([baseUrl, contactPath].join('/')).then((response) => response.json());
+  const getContacts = () => {
+  if(localStorage['contacts'] === undefined) {
+    localStorage['contacts'] = '';
+  } else {
+     let contactList = window.localStorage.getItem('contacts')
+     console.log(contactList);
+    //  return JSON.parse(contactList);
+  }
+}
+    // fetch([baseUrl, contactPath].join("/")).then((response) => response.json());
 
+  const addContact = (contact) => window.localStorage.setItem('contacts', JSON.stringify(contact))
+    // fetch([baseUrl, contactPath].join("/"), {
+    //   method: "POST",
+    //   body: JSON.stringify(contact),
+    //   headers: {
+    //     "Content-type": "application/json;charset=UTF-8",
+    //   },
+    // }).then((response) => response.json());
 
-    
-
-  const addContact = (contact) => 
-    fetch([baseUrl, contactPath].join('/'), {
-      method: 'POST',
-      body: JSON.stringify(contact),
-      headers: {
-        'Content-type': 'application/json;charset=UTF-8',
-      }
-    }).then((response) => response.json());
 
 
   return {
     getContacts,
-    addContact
-  }
+    addContact,
+  };
 })();
--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-import { Api } from "./api.js";
-
 /* View */
 const View = (() => {
   const domstr = {
     contactContainer: "#contactsList_container",
     submitBtn: ".btn",
-    // nameBox: "#name",
-    // phoneBox: "#mobile",
-    // emailBox: "#email",
   };
 
   const render = (ele, tmp) => {
@@ -35,10 +30,12 @@ const View = (() => {
 })();
 
 /* Model */
-const Model = ((api, view) => {
+// const Model = ((api, view) => {
+  const Model = ((view) => {
+
   class Contact {
     constructor(name, mobile, email) {
-      this.contactId = 1;
+      this.contactId = Math.random();
       this.contactName = name;
       this.contactMobile = mobile;
       this.contactEmail = email;
@@ -61,15 +58,17 @@ const Model = ((api, view) => {
     }
   }
 
-  const { getContacts, addContact } = api;
+  // const { getContacts, addContact } = api;
 
   return {
-    getContacts,
-    addContact,
+    // getContacts,
+    // addContact,
     Contact,
     State,
   };
-})(Api, View);
+})(View);
+// })(Api, View);
+
 
 /* Controller */
 const Controller = ((model, view) => {
@@ -79,29 +78,31 @@ const Controller = ((model, view) => {
     const submitButton = document.querySelector(view.domstr.submitBtn);
     submitButton.addEventListener('click', (event) => {
       event.preventDefault();
-      const name = document.querySelector('#name').trim();
-      const mobile = document.querySelector('#mobile').trim();
-      const email = document.querySelector('#email').trim();
-      
+
+      const name = document.querySelector('#name').value.trim();
+      const mobile = document.querySelector('#mobile').value.trim();
+      const email = document.querySelector('#email').value.trim();
+
       const newContact = new model.Contact(name, mobile, email);
       console.log(newContact);
 
-      model.addContact(newContact).then(contact => {
-        state.contactList = [contact, ...state.contactList];
-      })
+      state.contactList.push(newContact);
+      console.log(state.contactList);
 
       // clear the input box
-      name.value = '';
-      mobile.value = '';
-      email.value = '';
+      // name.value = '';
+      // mobile.value = '';
+      // email.value = '';
+     
     })
   }
 
-  const init = () => {
-    model.getContacts().then(contacts => {
-      state.contactList = [...contacts.reverse()];
-    })
-  }
+  const init = () => state.contactList.reverse();
+  // {
+  //   // model.getContacts().then(contacts => {
+  //     // state.contactList = [...contacts.reverse()];
+  //   // })
+  // }
 
   const bootstrap = () => {
     init();
